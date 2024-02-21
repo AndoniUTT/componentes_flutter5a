b21c6ee23d9bb68244d21394bccdb38178674ac9
AndoniUTT <andoni2020lopez@gmail.com>
2024-02-21 11:56:39 -0600

    Adding infite, inpust and notification class

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practica3/theme/app_theme.dart';
+import 'package:practica3_5a24/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,34 +14,40 @@ class HomeScreen extends StatelessWidget {
           children: <Widget> [
             ListTile(
               title: Text('Entradas',
-                  style: AppTheme.lightTheme.textTheme.headlineLarge,
+              style: AppTheme.lightTheme.textTheme.headlineLarge
                   ),
               subtitle: Text('Recuperar contraseñas',
-                style: Theme.of(context).textTheme.bodySmall,
+              style: AppTheme.lightTheme.textTheme.bodySmall
                 ),
-              leading: const Icon(Icons.input_rounded),
-              trailing: const Icon(Icons.arrow_right_alt_outlined),
+              leading: const Icon(Icons.input_rounded,
+               color: AppTheme.primaryColor),
+              trailing: const Icon(Icons.arrow_right_alt_outlined,
+               color: AppTheme.secondaryColor),
             ),
             const Divider(),
             ListTile(
               title: Text('Lista infinita',
-                style: Theme.of(context).textTheme.headlineLarge,
+              style: AppTheme.lightTheme.textTheme.headlineLarge
                 ),
               subtitle: Text('Recuperar muchos elementos',
                 style: Theme.of(context).textTheme.bodySmall,
                 ),
-              leading: const Icon(Icons.list_alt_sharp),
-              trailing: const Icon(Icons.arrow_right_outlined),
+              leading: const Icon(Icons.list_alt_sharp,
+               color: AppTheme.primaryColor),
+              trailing: const Icon(Icons.arrow_right_outlined,
+               color: AppTheme. secondaryColor),
             ),
             ListTile(
               title: Text('Notificaciones',
-                style: Theme.of(context).textTheme.headlineLarge,
+              style: AppTheme.lightTheme.textTheme.headlineLarge
                 ),
               subtitle: Text('Manejo de notificaciones',
-                style: Theme.of(context).textTheme.bodySmall,
+              style: AppTheme.lightTheme.textTheme.headlineLarge
                 ),
-              leading: const Icon(Icons.notification_important),
-              trailing: const Icon(Icons.arrow_right_outlined),
+              leading: const Icon(Icons.notification_important,
+               color: AppTheme.primaryColor),
+              trailing: const Icon(Icons.arrow_right_outlined,
+               color: AppTheme.secondaryColor),
             )
             ],
           ),
